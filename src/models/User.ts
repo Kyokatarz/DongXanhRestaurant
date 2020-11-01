6551bc592fc59870ee87795f95eb147c3d1e1341
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-11-01 19:34:52 +0200

    Add user Schema

---
+++ src/models/User.ts
@@ -0,0 +1,33 @@
+import mongoose, { Document } from 'mongoose'
+
+export type UserDocument = Document & {
+  username: string
+  password: string
+  email: string
+  firstName?: string
+  lastName?: string
+  isAdmin: boolean
+}
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  firstName: String,
+  lastName: String,
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+export default mongoose.model<UserDocument>('user', UserSchema)
