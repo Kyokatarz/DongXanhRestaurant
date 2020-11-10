051aaf5cc85e0efaa267889394b74f4bf206afba
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2020-11-10 18:45:04 +0200

    Add material UI and play around a bit

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,56 +1,31 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import {
+  Button,
+  createMuiTheme,
+  ThemeProvider,
+  Card,
+  Typography,
+  CssBaseline,
+  withStyles,
+  AppBar,
+  Toolbar,
+} from '@material-ui/core'
 
-import { Product, AppState } from '../types'
-import { addProduct, removeProduct } from '../redux/actions'
-
-const names = [
-  'Apple',
-  'Orange',
-  'Avocado',
-  'Banana',
-  'Cucumber',
-  'Carrot',
-]
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#C22828',
+    },
+  },
+})
 
 export default function Home() {
-  const dispatch = useDispatch()
-  const products = useSelector((state: AppState) => state.product.inCart)
-
-  const handleAddProduct = () => {
-    const product: Product = {
-      id: (+new Date()).toString(),
-      name: names[Math.floor(Math.random() * names.length)],
-      price: +(Math.random() * 10).toFixed(2),
-    }
-    dispatch(addProduct(product))
-  }
-
   return (
     <>
-      <h1>Home page</h1>
-      { products.length <= 0 && <div>No products in cart</div> }
-      <ul>
-        { products.map(p => (
-          <li key={p.id}>
-            <Link to={`/products/${p.id}`}>
-              {`${p.name} - $${p.price}`}
-            </Link>
-
-            {'  '}
-
-            <button onClick={ () => dispatch(removeProduct(p)) }>
-              Remove
-            </button>
-          </li>
-        ))}
-      </ul>
-      <button
-        onClick={ handleAddProduct }
-      >
-        Add product
-      </button>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </>
   )
 }
